f686ca76a8ff487fa065afe201d71a58df7fbe5d
movavi_school <bazauera@gmail.com>
2023-01-24 21:28:37 +0700

    24 января

---
+++ Урок 18/script.js
@@ -0,0 +1,44 @@
+// let message; //Выявили переменную
+// message = 'Hello'; //присвоили этой переменной значение
+// alert(message); //вывели ее
+// console.log(message)
+// console.log(typeof $)
+// console.log(typeof 20)
+// console.log(typeof _)
+// console.log()
+// let type;
+// type = '42'
+// message = '0n';
+// console.log(message)
+// message = "world";
+// console.log(`hello$(message)`);
+
+
+// let color = prompt("Какой твой любимый цвет?");
+// alert(`Пользователь ответил - ${ color }`);
+
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// alert(+a + +b);
+
+
+
+//  let m = prompt("Какой твой вес?");
+//  let h = prompt("Какой твой рост?");
+//  alert(m/(h/100)**2);
+
+//  let a = prompt("Какой вес твоего друга?");
+//  let b = prompt("Какой рост твоего друга?");
+//  alert(a/(b/100)**2);
+
+// alert(1>0);
+
+const name = "Иван";
+const job = "учитель";
+const fjb = 2014;
+const today = 2023;
+console.log("Прошу внимания, это ваш новый" + job, name + ".Его стаж работы " + (today - fjb) + " лет" )
+
+
+
