d955a515a0763e528dc6cf6ca6b63224b830fb22
movavi_school <bazauera@gmail.com>
2023-02-07 21:23:30 +0700

    7 Февраля

---
+++ Урок 20/Script.js
@@ -0,0 +1,85 @@
+"use strict";
+// function logger(){
+//     console.log('Всем привет');
+// }
+// logger();
+// if (oranges === undefined){
+//     const juice=`Ваш сок из ${apple} яблок и ${oranges} апельсинов готов!`
+//     oranges = "Без использования";
+// }
+// function juiceMaker(apple, oranges){
+//     const juice=`Ваш сок из ${apple} яблок и ${oranges} апельсинов готов!`
+//     return juice;
+// }
+// console.log(juiceMaker(2,3))
+
+// function juiceMaker(apples, oranges){
+//     oranges = oranges || "Без использования";
+//     console.log(apples, oranges);
+//     const juice = `Ваш сок из ${apple} яблок и ${oranges} апельсинов готов!`
+//     return juice;
+// }
+// console.log(juiceMaker(6));
+
+// let a = prompt("введите первое число")
+// let b = prompt("введите второе число")
+// function number(a,b){
+//      if(a>b) return b;
+//      return a;
+// }
+
+
+// function calcAge1() {
+//     return 2023 - birthYear;
+// }
+// const age1 = calcAge1() ;
+// console.log (age1) ; 
+
+
+
+// const calcAge2 = function () {
+//     return 2023 - birthYear;
+// };                                  
+// const age2 = calcAge2();
+// console.log(age1, age2);
+
+// function comands(){
+//     let a = 44
+//     let b = 23
+//     let c = 71
+//     let d = 0
+//     let e = 3
+//     let f = 65
+//     let g = 54
+//     let k = 49
+//     let q = 0
+//     d = ((a+b+c)/e);
+//      q = ((f+g+k)/e);
+//      if(d<q){
+//         alert("Первая команда выиграла");
+//      }
+     
+// }
+// let comand1[44, 23, 71]
+// let comand2[65, 54, 49]
+
+
+const name = [" ", " ", " "];
+const student = ["Саша", "Дима", "Бобик", "Елена", "Михаил", ]
+console.log(student);
+
+console.log(student[2])
+
+console.log( student[student.length -1])
+console.log(student.at(-4))
+
+const massiv = ["Элемент1"];
+massiv[3] = "Элемент2";
+
+
+console.log(massiv);
+
+const firstName= 'Глеб';
+const teacherInfo = [firstName, 2023-2008, "ученик", student];
+console.log(teacherInfo);
+Слайд 44
