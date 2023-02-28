c4448b46c824e372a19957cd83c0780153e09c82
movavi_school <gleb.meleshlo.99@mail.ru>
2023-02-28 21:26:13 +0700

    28 Февраля

---
+++ Урок 23/Script.js
@@ -0,0 +1,50 @@
+
+// for( let jump=1; jump<=10; jump++){
+//     if(jump == 4) continue; //пропуск интерации
+//     console.log("Прыг" + jump);
+// };
+
+// for( let jump=1; jump<=10; jump++){
+//     if(jump == 4) break; //выход из цикла
+//     console.log("Прыг" + jump);
+// };
+
+// for( let jump=1; jump<=10; jump++){
+//     if( jump == 4 )continue;
+//     console.log("Прыг", jump);
+//     for( let squat=1; squat<=3; squat++) {
+//         if(jump % 2)
+//         console.log("Присед", squat);
+        
+// }};
+
+
+// let jump = 1
+// while(jump<=10){
+//     console.log("Прыг", jump);
+//     jump++;
+// }
+
+
+
+// x = Math.trunc (Math.random() * 6) + 1
+// console.log(x)
+// console.log(Math.trunc(x)) //Округление вниз
+// console.log(Math.ceil(x)) //Округление вверх
+// console.log(Math.round(x)) //математическое округление
+// console.log(x.toFixed(100))
+
+// console.log(1.35.toFixed(20));
+// console.log(6.35.toFixed(20))
+
+
+
+document.querySelector(".message").innerText = "Другое сообщение"
+// document.quertySelector(".message").style.backgroundColor ="blue"
+
+document.querySelector(".message").innerText = "Другое сообщение"
+
+
+
+
+
