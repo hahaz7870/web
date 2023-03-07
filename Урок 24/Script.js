f0091c7cb29323255c6623c63c6d41275dbd2828
movavi_school <varvara.v.korshunova@gmail.com>
2023-03-07 21:26:39 +0700

    7 Марта

---
+++ Урок 24/Script.js
@@ -0,0 +1,8 @@
+// let num = 3.1415
+// console.log(num.toFixed(3))
+// let inp = document.querySelector(".chiki-piki");
+// inp.value = "Прив"
+
+// function myFunction(){
+//   document.querySelector(".chiki-piki").value = "Прив"
+// }
