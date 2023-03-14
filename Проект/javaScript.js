a631d7c26d5d89b365a3012639bc72f083bfea4c
movavi_school <mdiscool1@mail.ru>
2023-03-14 21:29:48 +0700

    14 Марта

---
+++ Проект/javaScript.js
@@ -0,0 +1,23 @@
+let input = document.querySelector(".text")
+let btn = document.querySelector(".button-text")
+
+
+
+// let computer = Math.trunc(1 + rand) //1 - 100
+// let user = document.querySelector(".in1").value
+// let counter = document.querySelector(".count")
+// let life = 15
+
+// //счет = жизни
+// if(computer == user){
+
+// }
+// else if(computer < user){
+//    life--
+//    counter.innerHTML = life
+// }
+
+// else if(computer < user){
+//    life--
+   
+// }
